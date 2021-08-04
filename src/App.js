f5766d58994418aab34ce6a39e2334d31f77a9e2
Dmitriy Poplinski <dmytro.poplinski@gmail.com>
2021-08-04 14:57:14 +0300

    Added new environment variable and realized firebaseReducer

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import { About } from './pages/About';
 import { Navbar } from './components/Navbar';
 import { Alert } from './components/Alert';
 import { AlertState } from './context/alert/alertState';
+import { FirebaseState } from './context/firebase/firebaseState';
 
 function App() {
   return (
-    <AlertState>
-    <BrowserRouter>
-    <Navbar />
-      <div className='container pt-4'>
-        <Alert />
-        <Switch>
-          <Route path={'/'} exact component={Home}/>
-          <Route path={'/about'} exact component={About}/>
-          <Route />
-        </Switch>
-      </div>
-    </BrowserRouter>
-    </AlertState>
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+        <Navbar />
+          <div className='container pt-4'>
+            <Alert />
+            <Switch>
+              <Route path={'/'} exact component={Home}/>
+              <Route path={'/about'} exact component={About}/>
+              <Route />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   );
 }
 
--- src/context/firebase/firebaseReducer.js
+++ src/context/firebase/firebaseReducer.js
@@ -1,5 +1,20 @@
+import { ADD_NOTE, SHOW_LOADER, FETCH_NOTES, REMOVE_NOTE } from './../types';
+
 const handlers = {
-    DEFAULT: state => state
+    [SHOW_LOADER]: state => ({...state, loading: true}),
+    [ADD_NOTE]: (state, {payload}) => ({
+        ...state, 
+        notes: [...state.notes, payload]
+    }),
+    [FETCH_NOTES]: (state, {payload}) => ({
+        ...state,
+        notes: payload
+    }),
+    [REMOVE_NOTE]: (state, payload) => ({
+        ...state,
+        notes: state.notes.filter(note => note.id !== payload)
+    }),
+    DEFAULT: state => state,
 }
 
 export const firebaseReducer = (state, action) => {
