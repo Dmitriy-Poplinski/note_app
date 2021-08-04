09615aa13d7ec191fb74c4c95a5a211b8d417d61
Dmitriy Poplinski <dmytro.poplinski@gmail.com>
2021-08-04 15:14:21 +0300

    Created and realized Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+
+export function Loader() {
+    return (
+        <div className='text-center'>
+            <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        </div>
+    )
+}
