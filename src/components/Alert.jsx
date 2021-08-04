5a35d52838174e7f4a1c9a7128abf580170ae047
Dmitriy Poplinski <dmytro.poplinski@gmail.com>
2021-08-04 16:33:48 +0300

    Added animation to Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,22 +1,30 @@
 import { AlertContext } from './../context/alert/alertContext';
 import { useContext } from 'react';
+import {CSSTransition} from 'react-transition-group'
 
 export function Alert() {
     const {alert, hide} = useContext(AlertContext)
 
-    if (!alert.visible) {
-        return null
-    }
-
     return  (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`} role="alert">
-            <div className='alert-info-wrapper'>
-                <strong>Внимание!</strong> 
-                <p>&nbsp;&nbsp;{alert.text}</p>
+        <CSSTransition
+            in={alert.visible}
+            timeout={{
+                enter: 500,
+                exit: 350,
+            }}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`} role="alert">
+                <div className='alert-info-wrapper'>
+                    <strong>Внимание!</strong> 
+                    <p>&nbsp;&nbsp;{alert.text}</p>
+                </div>
+                <button type="button" onClick={hide} className="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <button type="button" onClick={hide} className="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+        </CSSTransition>
     )
 }
